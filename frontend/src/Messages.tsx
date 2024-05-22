9efa2b5a3b96dd5495061226b7ad65400168ec99
Philipp Renner <phillianus@gmail.com>
2024-05-22 13:18:10 +0200

    refactor(frontend): add scrollarea for messages

--- frontend/src/Messages.tsx
+++ frontend/src/Messages.tsx
@@ -2,17 +2,19 @@ import { useEffect, useRef } from 'react';
 import { ScrollArea } from './components/ui/scroll-area';
 
 function Messages({ messages }: { messages: SharedTypes.Message[] }) {
-    const chatContainerRef = useRef<HTMLDivElement>(null);
+    const newestMessageRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        if (!chatContainerRef.current) return;
-        chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        if (!newestMessageRef.current) return;
+        newestMessageRef.current.scrollIntoView();
     }, [messages]);
 
     return (
-        <ScrollArea className="flex-1 h-full px-4 py-2 my-4 border-2 bg-gray-100 border-gray-200 rounded-md dark:bg-gray-800 dark:border-gray-700 text-left break-words">
-            {messages.map(({ id, userId, message }) => (
-                <div key={id}>{userId + ': ' + message}</div>
+        <ScrollArea className="flex-1 h-full px-4 py-2 my-4 border-2 bg-gray-100 border-gray-200 rounded-md dark:bg-gray-800 dark:border-gray-700 text-left">
+            {messages.map(({ userId, message }, id) => (
+                <div key={id} ref={messages.length - 1 === id ? newestMessageRef : null}>
+                    {userId + ': ' + message}
+                </div>
             ))}
         </ScrollArea>
     );
