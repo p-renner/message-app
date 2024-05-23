eaeaca1cebe9d2cb6e65ec1b7b5bbe39f248f444
Philipp Renner <phillianus@gmail.com>
2024-05-23 07:38:19 +0200

    fix(messages): fix overflowing messages

--- frontend/src/Messages.tsx
+++ frontend/src/Messages.tsx
@@ -10,12 +10,17 @@ function Messages({ messages }: { messages: SharedTypes.Message[] }) {
     }, [messages]);
 
     return (
-        <ScrollArea className="flex-1 h-full px-4 py-2 my-4 border-2 bg-gray-100 border-gray-200 rounded-md dark:bg-gray-800 dark:border-gray-700 text-left">
-            {messages.map(({ userId, message }, id) => (
-                <div key={id} ref={messages.length - 1 === id ? newestMessageRef : null}>
-                    {userId + ': ' + message}
-                </div>
-            ))}
+        <ScrollArea
+            className="flex-1 h-full px-4 py-2 my-4 border-2 bg-gray-100 border-gray-200 rounded-md dark:bg-gray-800 dark:border-gray-700 text-left"
+            role="feed"
+        >
+            <div className="table table-fixed w-full">
+                {messages.map(({ userId, message }, id) => (
+                    <p className="break-words" key={id} ref={messages.length - 1 === id ? newestMessageRef : null}>
+                        {userId + ': ' + message}
+                    </p>
+                ))}
+            </div>
         </ScrollArea>
     );
 }
