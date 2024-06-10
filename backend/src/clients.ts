6b83b8a3a819ff90a301280f8f42bceb421dc9e6
Philipp Renner <phillianus@gmail.com>
2024-06-10 19:46:10 +0000

    fix(ws): use unique channel for map key

--- backend/src/clients.ts
+++ backend/src/clients.ts
@@ -1,13 +1,23 @@
 import * as ws from 'ws';
-import { Channel } from './models/channels.models';
-const clients = new Map<Channel, Set<ws>>();
+const clients = new Map<string, Set<ws>>();
 
-export function addClient(channel: Channel, client: ws) {
-    if (clients.has(channel)) {
-        clients.get(channel)!.add(client);
-    } else {
+export function addClient(channel: string, client: ws): void {
+    if (!clients.has(channel)) {
         clients.set(channel, new Set([client]));
+        return;
     }
+
+    getClients(channel).add(client);
 }
 
-export default clients;
+export function deleteClient(channel: string, client: ws): void {
+    clients.get(channel)?.delete(client);
+}
+
+export function getClients(channel: string): Set<ws> {
+    if (!clients.has(channel)) {
+        return new Set<ws>();
+    }
+
+    return clients.get(channel)!;
+}
