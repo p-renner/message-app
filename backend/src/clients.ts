a200e4b28d68986b6c6990bad0ef13c46b9aae13
Philipp Renner <phillianus@gmail.com>
2024-06-11 11:20:49 +0200

    refactor(clients): use factory function
    Avoid Singleton (global state) and use factory function for reusability,
    modularity and better testability

--- backend/src/clients.ts
+++ backend/src/clients.ts
@@ -1,23 +1,44 @@
 import * as ws from 'ws';
-const clients = new Map<string, Set<ws>>();
 
-export function addClient(channel: string, client: ws): void {
-    if (!clients.has(channel)) {
-        clients.set(channel, new Set([client]));
-        return;
-    }
-
-    getClients(channel).add(client);
+export interface ChannelManager {
+    addClient(channel: string, client: ws): void;
+    deleteClient(channel: string, client: ws): void;
+    getClients(channel: string): Set<ws>;
+    resetClients(): void;
 }
 
-export function deleteClient(channel: string, client: ws): void {
-    clients.get(channel)?.delete(client);
-}
+export function createChannelManager(): ChannelManager {
+    const clients = new Map<string, Set<ws>>();
 
-export function getClients(channel: string): Set<ws> {
-    if (!clients.has(channel)) {
-        return new Set<ws>();
+    function addClient(channel: string, client: ws): void {
+        if (!clients.has(channel)) {
+            clients.set(channel, new Set([client]));
+            return;
+        }
+
+        getClients(channel).add(client);
     }
 
-    return clients.get(channel)!;
+    function deleteClient(channel: string, client: ws): void {
+        clients.get(channel)?.delete(client);
+    }
+
+    function getClients(channel: string): Set<ws> {
+        if (!clients.has(channel)) {
+            return new Set<ws>();
+        }
+
+        return clients.get(channel)!;
+    }
+
+    function resetClients(): void {
+        clients.clear();
+    }
+
+    return {
+        addClient,
+        deleteClient,
+        getClients,
+        resetClients,
+    };
 }
